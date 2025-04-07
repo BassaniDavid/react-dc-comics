65083fed3a9417374108a5d3a1bd44e1dda559ca
BassaniDavid <davbassan98@gmail.com>
2025-04-07 14:49:56 +0200

    feat: added current series button

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,7 @@
 export default function Main() {
     return (
         <main>
+            <button className="btnmain">current series</button>
             <h2>&minus; &minus; &gt; Content goes here &lt; &minus; &minus;</h2>
         </main>
     );
